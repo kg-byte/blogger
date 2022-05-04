feb571124395e0dbfddca1ad5cf1237730de20b7
Kim G <xiaole.guo@yahoo.com>
2022-05-03 20:58:45 -0600

    user sees one article when they visit article_path

--- spec/features/user_see_all_articles_spec.rb
+++ spec/features/user_see_all_articles_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 describe 'user see all articles' do 
   describe 'they visit /articles' do 
+  	 let!(:article_1) {create(:article)}
+  	 let!(:article_2) {create(:article)}
+
   	it 'displays all articles' do 
-  	  article_1 = create(:article)
-  	  article_2 = create(:article)
   	 
-  	  visit '/articles'
+  	  visit articles_path
 
   	  expect(page).to have_content(article_1.title)
   	  expect(page).to have_content(article_2.title)
+  	  expect(page).to have_link(article_1.title)
+  	  expect(page).to have_link('Create a New Article')
   	end
+
+
+
   end
 end
