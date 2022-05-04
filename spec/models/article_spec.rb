09e566af0aacd50da7c290ede9f2af95997e0956
Kim G <xiaole.guo@yahoo.com>
2022-05-03 20:15:26 -0600

    Add Article migration

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-describe Article, type: :model do 
-  describe 'validations' do 
-  	it { should validate_presence_of(:title)}
-  	it { should validate_presence_of(:body)}
-
+RSpec.describe Article, type: :model do
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:body)}
 end
